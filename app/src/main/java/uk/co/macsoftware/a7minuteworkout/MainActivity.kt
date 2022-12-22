6bc39edd7a7ee8d2d668a81c3083e0e70df10164
macsoftware <87543117+macsoftware@users.noreply.github.com>
2022-12-22 11:24:24 +0000

    Making start on the UI

--- app/src/main/java/uk/co/macsoftware/a7minuteworkout/MainActivity.kt
+++ app/src/main/java/uk/co/macsoftware/a7minuteworkout/MainActivity.kt
@@ -2,10 +2,23 @@ package uk.co.macsoftware.a7minuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val frameLayoutStartButton: FrameLayout = findViewById(R.id.frameLayoutStart)
+        frameLayoutStartButton.setOnClickListener{
+            Toast.makeText(
+                this,
+                "Here we will start the exercise.",
+                Toast.LENGTH_LONG
+            ).show()
+        }
     }
 }
